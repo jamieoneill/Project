2f7f5e6525b7f23a3a46a21d416e27ad7f630a69
jamie <jamie-o-n07@hotmail.com>
2016-10-26 21:50:30 +0100

    Meeting locations now appear on map in realtime

--- app/src/main/java/com/mismatched/nowyouretalking/MapsActivity.java
+++ app/src/main/java/com/mismatched/nowyouretalking/MapsActivity.java
@@ -1,7 +1,12 @@
 package com.mismatched.nowyouretalking;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +14,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    double lat =0 , lng = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +38,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
+    //Convert to get address
+    public LatLng getLocationFromAddress(Context context, String strAddress)
+    {
+        Geocoder coder= new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try
+        {
+            address = coder.getFromLocationName(strAddress, 5);
+            if(address==null)
+            {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            p1 = new LatLng(location.getLatitude(), location.getLongitude());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return p1;
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,8 +83,56 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng address = getLocationFromAddress(this, "27, Flemington Park, Balbriggan, Dublin");
+        mMap.addMarker(new MarkerOptions().position(address).title("home").snippet("this is a snippet"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(address, 10));
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("Meetings");
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //retrieves all in meeting database
+                Map<String,Object> dbRead=(Map<String,Object>)dataSnapshot.getValue();
+                List<String> myList = new ArrayList<String>();
+
+                for (DataSnapshot child: dataSnapshot.getChildren()) {
+                    //get location from DB
+                    String Titles = child.child("Title").getValue(String.class);
+                    String Locations = child.child("Location").getValue(String.class);
+
+                    //add to maker here
+                    LatLng newaddress = getLocationFromAddress(MapsActivity.this, Locations);
+                    mMap.addMarker(new MarkerOptions().position(newaddress).title(Titles).snippet("this is a snippet"));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.i("database ", "Failed to read value.", error.toException());
+            }
+        });
+
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng mapclick) {
+                // TODO Auto-generated method stub
+                Log.d("mapclick", mapclick.latitude + "-" + mapclick.longitude);
+            }
+        });
+
+
     }
 }
