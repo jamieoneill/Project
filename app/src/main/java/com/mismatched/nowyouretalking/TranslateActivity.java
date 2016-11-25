77e0a10d7a069e59692f3fe128617a3f0c57458a
jamie <jamie-o-n07@hotmail.com>
2016-11-25 21:22:24 +0000

    Added text to speech functions on translate activity. all languages supported now have individual speech voices. added circlular button xlm. added catch to make sure location is entered on add a meet up. uploading dark images used for map

--- app/src/main/java/com/mismatched/nowyouretalking/TranslateActivity.java
+++ app/src/main/java/com/mismatched/nowyouretalking/TranslateActivity.java
@@ -1,22 +1,28 @@
 package com.mismatched.nowyouretalking;
 
+import android.annotation.TargetApi;
+import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import com.memetix.mst.language.Language;
 import com.memetix.mst.translate.Translate;
+import java.util.Locale;
 
 /**
  * Created by jamie on 24/11/2016.
  */
 
-public class TranslateActivity extends AppCompatActivity {
+public class TranslateActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,12 @@ public class TranslateActivity extends AppCompatActivity {
         final Spinner FromLanguageSpinner = (Spinner) findViewById(R.id.FromLanguageSpinner);
         final Spinner ToLanguageSpinner = (Spinner) findViewById(R.id.ToLanguageSpinner);
 
+        //set authentication
+        Translate.setClientId("NowYoureTalking");
+        Translate.setClientSecret("nowyourtalkingsecret");
+
+        //set text to speech
+        tts = new TextToSpeech(this, this);
 
         Button translateButton = (Button) findViewById(R.id.translateButton);
         translateButton.setOnClickListener(new View.OnClickListener() {
@@ -36,11 +48,8 @@ public class TranslateActivity extends AppCompatActivity {
 
                 //must run on separate thread
                 new Thread(){
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
                     public void run() {
-
-                        //set authentication
-                        Translate.setClientId("NowYoureTalking");
-                        Translate.setClientSecret("nowyourtalkingsecret");
 
                         //get selected languages
                         String fromLanguage = String.valueOf(FromLanguageSpinner.getSelectedItem()).toUpperCase();
@@ -50,8 +59,10 @@ public class TranslateActivity extends AppCompatActivity {
                         String originalText  =  InputText.getText().toString();
 
                         try {
+                            //return translation
                             String translatedText = Translate.execute(originalText, Language.valueOf(fromLanguage), Language.valueOf(toLanguage));
                             ResultLabel.setText(translatedText);
+
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -63,7 +74,73 @@ public class TranslateActivity extends AppCompatActivity {
             }
         });
 
+        ImageButton SpeakButton = (ImageButton) findViewById(R.id.SpeakButton);
+        SpeakButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //call speak method
+                String toLanguage = String.valueOf(ToLanguageSpinner.getSelectedItem()).toUpperCase();
+                speakOut(ResultLabel.getText().toString(), toLanguage);
+
+            }
+        });
 
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void speakOut(String input, String language) {
+
+        Locale speechLanguage;
+
+        // set voice to destination language
+        if(language.equals("SPANISH")){
+            speechLanguage = new Locale("spa", "MEX");
+            tts.setLanguage(speechLanguage);
+        }
+        else if(language.equals("FRENCH")){
+            speechLanguage =  Locale.FRANCE;
+            tts.setLanguage(speechLanguage);
+        }
+        else if(language.equals("GERMAN")){
+            speechLanguage = Locale.GERMAN;
+            tts.setLanguage(speechLanguage);
+        }
+        else if(language.equals("ENGLISH")){
+            speechLanguage = Locale.ENGLISH;
+            tts.setLanguage(speechLanguage);
+        }
+
+
+        String utteranceId=this.hashCode() + "";
+            //speak text.. different for version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(input, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+        }
+        {
+            tts.speak(input, TextToSpeech.QUEUE_FLUSH, null);
+       }
+
     }
+
+    @Override
+    public void onInit(int status) {
+        // catch  tts error
+        if (status != TextToSpeech.SUCCESS) {
+            Log.e("TTS", "Initilization Failed!");
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+
+        //Close the Text to Speech Library
+        if(tts != null) {
+
+            tts.stop();
+            tts.shutdown();
+            Log.i("onDestroy", "TTS Destroyed");
+        }
+        super.onDestroy();
+    }
+}
