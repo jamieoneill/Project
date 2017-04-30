6fdc884ba365aa695c6f7d4afb854b25554688ca
jamie <jamie-o-n07@hotmail.com>
2017-04-30 21:16:57 +0100

    Updated entire navbar including images. removed scrolling animation

--- app/src/main/java/com/mismatched/nowyouretalking/NavbarFragment.java
+++ app/src/main/java/com/mismatched/nowyouretalking/NavbarFragment.java
@@ -8,9 +8,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 
 /**
  * Created by jamie on 27/11/2016.
@@ -22,138 +21,72 @@ public class NavbarFragment extends Fragment {
         // Inflating the layout for this fragment
         final View view = inflater.inflate(R.layout.navbar_fragment, null);
 
+        //buttons
+        final ImageButton SocialButton = (ImageButton) view.findViewById(R.id.SocialButton);
+        final ImageButton LearnButton = (ImageButton) view.findViewById(R.id.LearnButton);
+        final ImageButton profileButton = (ImageButton) view.findViewById(R.id.ProfileButton);
+        ImageButton messagingButton = (ImageButton) view.findViewById(R.id.MessagingActivityButton);
 
-    Button socialButton = (Button) view.findViewById(R.id.SocialButton);
-    socialButton.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            //change fragment view
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            SocialFragment social = new SocialFragment();
-            fragmentTransaction.replace(R.id.fragment_container, social, "Social");
-            fragmentTransaction.commit();
+        SocialButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-            //get previous fragment
-            Fragment previousFragment = fragmentManager.findFragmentById(R.id.fragment_container);
-            String fragTag = previousFragment.getTag();
+                //set buttons
+                SocialButton.setBackgroundResource(R.drawable.ic_social_selected);
+                LearnButton.setBackgroundResource(R.drawable.ic_learn);
+                profileButton.setBackgroundResource(R.drawable.ic_profile);
 
-            //set image and animation
-            ImageView highligher = (ImageView) view.findViewById(R.id.highligherImage);
-            TranslateAnimation highligherAnimation;
-
-            //animate slider image based on previous frag
-            switch (fragTag){
-                case "Learn":
-                    highligherAnimation = new TranslateAnimation(360, 0, 0 , 0);
-                    highligherAnimation.setDuration(200);
-                    highligherAnimation.setFillAfter(true);
-                    highligherAnimation.setFillEnabled(true);
-
-                    highligher.startAnimation(highligherAnimation);
-                    break;
-                case "Profile":
-                    highligherAnimation = new TranslateAnimation(720, 0, 0 , 0);
-                    highligherAnimation.setDuration(200);
-                    highligherAnimation.setFillAfter(true);
-                    highligherAnimation.setFillEnabled(true);
-
-                    highligher.startAnimation(highligherAnimation);
+                //change fragment view
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                SocialFragment social = new SocialFragment();
+                fragmentTransaction.replace(R.id.fragment_container, social, "Social");
+                fragmentTransaction.commit();
             }
-        }
-    });
+        });
 
-    Button learnButton = (Button) view.findViewById(R.id.LearnButton);
-    learnButton.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            //change fragment view
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            LearnFragment learn = new LearnFragment();
-            fragmentTransaction.replace(R.id.fragment_container, learn, "Learn");
-            fragmentTransaction.commit();
+        LearnButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-            //get previous fragment
-            Fragment currentFragment = fragmentManager.findFragmentById(R.id.fragment_container);
-            String fragTag = currentFragment.getTag();
+                //set buttons
+                SocialButton.setBackgroundResource(R.drawable.ic_social);
+                LearnButton.setBackgroundResource(R.drawable.ic_learn_selected);
+                profileButton.setBackgroundResource(R.drawable.ic_profile);
 
-            //set image and animation
-            ImageView highligher = (ImageView) view.findViewById(R.id.highligherImage);
-            TranslateAnimation highligherAnimation;
-
-            //animate slider image based on previous frag
-            switch (fragTag){
-                case "Social":
-                    highligherAnimation = new TranslateAnimation(0, 360, 0 , 0);
-                    highligherAnimation.setDuration(200);
-                    highligherAnimation.setFillAfter(true);
-                    highligherAnimation.setFillEnabled(true);
-
-                    highligher.startAnimation(highligherAnimation);
-                    break;
-                case "Profile":
-                    highligherAnimation = new TranslateAnimation(720, 360, 0 , 0);
-                    highligherAnimation.setDuration(200);
-                    highligherAnimation.setFillAfter(true);
-                    highligherAnimation.setFillEnabled(true);
-
-                    highligher.startAnimation(highligherAnimation);
+                //change fragment view
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                LearnFragment learn = new LearnFragment();
+                fragmentTransaction.replace(R.id.fragment_container, learn, "Learn");
+                fragmentTransaction.commit();
             }
-        }
-    });
+        });
 
-    Button profileButton = (Button) view.findViewById(R.id.ProfileButton);
-    profileButton.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            ProfileFragment profile = new ProfileFragment();
-            fragmentTransaction.replace(R.id.fragment_container, profile, "Profile");
-            fragmentTransaction.commit();
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-            //get previous fragment
-            Fragment currentFragment = fragmentManager.findFragmentById(R.id.fragment_container);
-            String fragTag = currentFragment.getTag();
+                //set buttons
+                SocialButton.setBackgroundResource(R.drawable.ic_social);
+                LearnButton.setBackgroundResource(R.drawable.ic_learn);
+                profileButton.setBackgroundResource(R.drawable.ic_profile_selected);
 
-            //set image and animation
-            ImageView highligher = (ImageView) view.findViewById(R.id.highligherImage);
-            TranslateAnimation highligherAnimation;
-
-            //animate slider image based on previous frag
-            switch (fragTag){
-                case "Social":
-                    highligherAnimation = new TranslateAnimation(0, 720, 0 , 0);
-                    highligherAnimation.setDuration(200);
-                    highligherAnimation.setFillAfter(true);
-                    highligherAnimation.setFillEnabled(true);
-
-                    highligher.startAnimation(highligherAnimation);
-                    break;
-                case "Learn":
-                    highligherAnimation = new TranslateAnimation(360, 720, 0, 0);
-                    highligherAnimation.setDuration(200);
-                    highligherAnimation.setFillAfter(true);
-                    highligherAnimation.setFillEnabled(true);
-
-                    highligher.startAnimation(highligherAnimation);
+                //change fragment view
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                ProfileFragment profile = new ProfileFragment();
+                fragmentTransaction.replace(R.id.fragment_container, profile, "Profile");
+                fragmentTransaction.commit();
             }
-        }
-    });
+        });
 
-        Button messagingButton = (Button) view.findViewById(R.id.MessagingActivityButton);
         messagingButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 //start message activity
                 Intent intent = new Intent(getActivity(), MessagingActivity.class);
                 startActivity(intent);
-
             }
         });
 
-
         return view;
     }
-
-
-
 }
